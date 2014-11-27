46cddb9134b4109cae38f90c0164c3761f532231
Joel Johnson <joeljohnson@attask.com>
2014-11-27 00:41:41 -0700

    Solidifying how work is executing. Making sure all exceptions are handled the way they should. Implementing before/after method calls for interceptors.

--- src/main/java/com/vanillaci/worker/model/WorkContextImpl.java
+++ src/main/java/com/vanillaci/worker/model/WorkContextImpl.java
@@ -2,6 +2,7 @@ package com.vanillaci.worker.model;
 
 import com.google.common.collect.*;
 import com.vanillaci.plugins.*;
+import com.vanillaci.worker.service.*;
 import jdk.nashorn.internal.ir.annotations.*;
 
 import java.util.*;
@@ -12,12 +13,16 @@ import java.util.*;
 public class WorkContextImpl implements WorkContext {
 	private final WorkMessage workMessage;
 	private final Map<String, String> parameters;
+	private final Map<String, String> addedParameters;
+	private WorkService.Status workStatus;
 	private WorkStep workStep;
 
-	public WorkContextImpl(WorkMessage workMessage, WorkStep workStep, Map<String, String> parameters) {
+	public WorkContextImpl(WorkMessage workMessage, WorkStep workStep, Map<String, String> parameters, Map<String, String> addedParameters, WorkService.Status currentStatus) {
 		this.workMessage = workMessage;
 		this.workStep = workStep;
-		this.parameters = ImmutableMap.copyOf(parameters);
+		this.parameters = parameters;
+		this.addedParameters = addedParameters;
+		this.workStatus = currentStatus;
 	}
 
 	@Override
@@ -32,7 +37,8 @@ public class WorkContextImpl implements WorkContext {
 
 	@Override
 	public void addParameter(String parameterName, String parameterValue) {
-		throw new UnsupportedOperationException("I should implement this now");
+		parameters.put(parameterName, parameterValue);
+		addedParameters.put(parameterName, parameterValue);
 	}
 
 	@Override
@@ -43,5 +49,32 @@ public class WorkContextImpl implements WorkContext {
 	@Override
 	public void setWorkStep(WorkStep workStep) {
 		this.workStep = workStep;
+	}
+
+	@Override
+	public WorkStatus getWorkStatus() {
+		return workStatus.getWorkStatus();
+	}
+
+	@Override
+	public void setWorkStatus(WorkStatus workStatus) {
+		if(getWorkStatus().isLessSevereThan(workStatus)) {
+			this.workStatus.setWorkStatus(workStatus);
+		}
+	}
+
+	@Override
+	public void overrideWorkStatus(WorkStatus workStatus) {
+		this.workStatus.setWorkStatus(workStatus);
+	}
+
+	@Override
+	public boolean getTerminate() {
+		return workStatus.getTerminate();
+	}
+
+	@Override
+	public void setTerminate(boolean terminate) {
+		workStatus.setTerminate(terminate);
 	}
 }
--- src/main/java/com/vanillaci/worker/service/WorkService.java
+++ src/main/java/com/vanillaci/worker/service/WorkService.java
@@ -23,19 +23,38 @@ public class WorkService {
 		logger.info("starting work: " + workMessage.getId());
 		try {
 			Map<String, String> workParameters = workMessage.getParameters();
+			Map<String, String> addedParameters = new HashMap<>();
+
+			Status status = new Status();
+			status.workStatus = WorkStatus.SUCCESS;
+			status.terminate = false;
 
 			try {
 				List<WorkStepMessage> steps = workMessage.getSteps();
 				for (WorkStepMessage step : steps) {
-					executeStep(workMessage, step, workParameters);
+					try {
+						executeStep(workMessage, step, workParameters, addedParameters, status);
+					} catch (Exception e) {
+						logger.info("Exception running work step: " + workMessage.getId() + " " + step.getName(), e);
+						status.setWorkStatus(WorkStatus.ERROR);
+						status.setTerminate(true);
+					}
+
+					if(status.terminate) {
+						logger.info("Terminating");
+						break;
+					}
 				}
 			} finally {
+				logger.info("Running post steps: " + workMessage.getId());
+
 				List<WorkStepMessage> postSteps = workMessage.getPostSteps();
 				for (WorkStepMessage postStep : postSteps) {
 					try {
-						executeStep(workMessage, postStep, workParameters);
+						executeStep(workMessage, postStep, workParameters, addedParameters, status);
 					} catch (Exception e) {
-						logger.info("Exception running post work step: " + workMessage.getId(), e);
+						logger.info("Exception running post work step: " + workMessage.getId() + " " + postStep.getName(), e);
+						status.setWorkStatus(WorkStatus.UNEXPECTED_ERROR);
 					}
 				}
 			}
@@ -44,20 +63,88 @@ public class WorkService {
 		}
 	}
 
-	private void executeStep(WorkMessage workMessage, WorkStepMessage step, Map<String, String> workParameters) {
+	private void executeStep(WorkMessage workMessage, WorkStepMessage step, Map<String, String> workParameters, Map<String, String> addedParameters, Status status) {
 		Map<String, String> stepParameters = step.getParameters();
-		ImmutableMap.Builder<String, String> builder = ImmutableMap.<String, String>builder();
-		builder.putAll(stepParameters);
 
-		workParameters.entrySet().stream()
-			.filter(entry -> !stepParameters.containsKey(entry.getKey()))
-			.forEach(entry -> builder.put(entry.getKey(), entry.getValue()));
-
-		ImmutableMap<String, String> allParameters = builder.build();
+		Map<String, String> allParameters = new HashMap<>();
+		allParameters.putAll(workParameters);
+		allParameters.putAll(stepParameters);
+		allParameters.putAll(addedParameters);
 
 		WorkStep workStep = pluginService.getWorkStep(step.getName());
-		WorkContext workContext = new WorkContextImpl(workMessage, workStep, allParameters);
+		WorkContext workContext = new WorkContextImpl(workMessage, workStep, allParameters, addedParameters, status);
 
-		workStep.execute(workContext);
+		Iterable<WorkStepInterceptor> workStepInterceptors = pluginService.getWorkStepInterceptors();
+
+		runBefores(workContext, workStepInterceptors);
+
+		try {
+			// Make sure the before didn't terminate the work
+			if(!workContext.getTerminate()) {
+				// Since the befores are allowed to change the step that is actually executed, make sure we execute that one.
+				WorkStep overwrittenStep = workContext.getWorkStep();
+				if(overwrittenStep != workStep) {
+					logger.info("Work step overwritten: " + workMessage.getId());
+				}
+
+				if(overwrittenStep != null) {
+					overwrittenStep.execute(workContext);
+				}
+			}
+		} catch (Exception e) {
+			workContext.setWorkStatus(WorkStatus.ERROR);
+			workContext.setTerminate(true);
+			throw e;
+		} finally {
+			runAfters(workContext, workStepInterceptors);
+		}
+	}
+
+	private void runBefores(WorkContext workContext, Iterable<WorkStepInterceptor> workStepInterceptors) {
+		for (WorkStepInterceptor workStepInterceptor : workStepInterceptors) {
+			try {
+				workStepInterceptor.before(workContext);
+			} catch (Exception e) {
+				logger.info("Exception running before " + workStepInterceptor.getClass().getName() + " for step " + workContext.getWorkStep().getClass().getName() + ": " + workContext.getWorkId(), e);
+				workContext.setWorkStatus(WorkStatus.ERROR);
+				workContext.setTerminate(true);
+			}
+		}
+	}
+
+	private void runAfters(WorkContext workContext, Iterable<WorkStepInterceptor> workStepInterceptors) {
+		for (WorkStepInterceptor workStepInterceptor : workStepInterceptors) {
+			try {
+				workStepInterceptor.after(workContext);
+			} catch (Exception e) {
+				logger.info("Exception running after " + workStepInterceptor.getClass().getName() + " for step " + workContext.getWorkStep().getClass().getName() + ": " + workContext.getWorkId(), e);
+				workContext.setWorkStatus(WorkStatus.ERROR);
+				workContext.setTerminate(true);
+			}
+		}
+	}
+
+	/**
+	 * Mutable object representing the work's current status.
+	 */
+	public static class Status {
+		private WorkStatus workStatus;
+		private boolean terminate;
+
+		public WorkStatus getWorkStatus() {
+			return workStatus;
+		}
+
+		public void setWorkStatus(WorkStatus workStatus) {
+			this.workStatus = workStatus;
+		}
+
+		public boolean getTerminate() {
+			return terminate;
+		}
+
+		public void setTerminate(boolean terminate) {
+			this.terminate = terminate;
+		}
 	}
 }
