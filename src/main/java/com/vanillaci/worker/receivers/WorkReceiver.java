13f2518dc10219091859a6d02a90d8deffb99e84
Joel Johnson <joeljohnson@attask.com>
2014-11-26 22:45:48 -0700

    properly handling exceptions in the WorkReceiver

--- src/main/java/com/vanillaci/worker/receivers/WorkReceiver.java
+++ src/main/java/com/vanillaci/worker/receivers/WorkReceiver.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.type.*;
 import com.fasterxml.jackson.databind.*;
 import com.vanillaci.worker.model.*;
 import com.vanillaci.worker.service.*;
+import org.apache.log4j.*;
 import org.springframework.beans.factory.annotation.*;
 
 import java.io.*;
@@ -12,13 +13,26 @@ import java.io.*;
  * @author Joel Johnson
  */
 public class WorkReceiver {
+	private Logger logger = Logger.getLogger(WorkReceiver.class);
+
 	@Autowired
 	private WorkService workService;
 
 	private ObjectMapper objectMapper = new ObjectMapper();
 
-	public void receiveMessage(String rawMessage) throws IOException {
-		WorkMessage message = objectMapper.readValue(rawMessage, new TypeReference<WorkMessage>() {});
-		workService.doWork(message);
+	public void receiveMessage(String rawMessage) {
+		WorkMessage message;
+		try {
+			message = objectMapper.readValue(rawMessage, new TypeReference<WorkMessage>() {});
+		} catch (Exception e) {
+			logger.fatal("Error while parsing work message", e);
+			return;
+		}
+
+		try {
+			workService.doWork(message);
+		} catch (Exception e) {
+			logger.fatal("Unhandled exception while executing work " + message.getId() + ". This is a bug and should be reported.", e);
+		}
 	}
 }
