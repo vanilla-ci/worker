3e287f93d725d0afbb8b87e03af7bbf5634baaa8
Joel Johnson <joeljohnson@attask.com>
2014-11-26 23:25:54 -0700

    dynamically load all plugins on the classpath

--- src/main/java/com/vanillaci/worker/service/PluginService.java
+++ src/main/java/com/vanillaci/worker/service/PluginService.java
@@ -1,13 +1,14 @@
 package com.vanillaci.worker.service;
 
 import com.google.common.collect.*;
-import com.vanillaci.dieplugin.*;
 import com.vanillaci.plugins.*;
-import com.vanillaci.echoplugin.*;
 import com.vanillaci.worker.exceptions.*;
+import org.apache.log4j.*;
 import org.springframework.stereotype.*;
 
 import javax.annotation.*;
+import java.io.*;
+import java.net.*;
 import java.util.*;
 
 /**
@@ -15,17 +16,56 @@ import java.util.*;
  */
 @Service
 public class PluginService {
+	private Logger logger = Logger.getLogger(PluginService.class);
+
 	private Map<String, WorkStep> workStepMap = ImmutableMap.of();
+	private Map<String, WorkStepInterceptor> workStepInterceptorMap = ImmutableMap.of();
 
 	@PostConstruct
-	private void init() {
-		registerPlugins(ImmutableList.of(
-			new EchoWorkStep(), // TODO: load jars dynamically
-			new DieWorkStep()
-		));
+	private void init() throws IOException {
+		ImmutableList.Builder<WorkStep> workStepBuilder = ImmutableList.builder();
+		ImmutableList.Builder<WorkStepInterceptor> workStepInterceptorBuilder = ImmutableList.builder();
+
+		Enumeration<URL> plugins = getClass().getClassLoader().getResources("vanillaci.plugins");
+		while(plugins.hasMoreElements()) {
+			URL url = plugins.nextElement();
+			try(InputStream inputStream = url.openStream()) {
+				Scanner scanner = new Scanner(inputStream);
+				int lineNumber = 0;
+				while(scanner.hasNextLine()) {
+					lineNumber++;
+					String line = scanner.nextLine();
+					try {
+						Class<?> loadedClass = Class.forName(line);
+
+						if(!WorkStep.class.isAssignableFrom(loadedClass) && !WorkStepInterceptor.class.isAssignableFrom(loadedClass)) {
+							continue;
+						}
+
+						Object instance = loadedClass.newInstance();
+						if(instance instanceof WorkStep) {
+							workStepBuilder.add((WorkStep) instance);
+						}
+
+						if(instance instanceof WorkStepInterceptor) {
+							workStepInterceptorBuilder.add((WorkStepInterceptor) instance);
+						}
+					} catch (ClassNotFoundException e) {
+						logger.error("Unabled to find declared plugin class from " + url.toString() + " line " + lineNumber + ": " + line, e);
+					} catch (InstantiationException e) {
+						logger.error("Unabled instantiate plugin from " + url.toString() + " line " + lineNumber + ": " + line, e);
+					} catch (IllegalAccessException e) {
+						logger.error("No public constructor for plugin from " + url.toString() + " line " + lineNumber + ": " + line, e);
+					}
+				}
+			}
+		}
+
+		registerWorkStepPlugins(workStepBuilder.build());
+		registerWorkStepInterceptorPlugins(workStepInterceptorBuilder.build());
 	}
 
-	public void registerPlugins(Iterable<WorkStep> workSteps) {
+	public void registerWorkStepPlugins(Iterable<WorkStep> workSteps) {
 		ImmutableMap.Builder<String, WorkStep> builder = ImmutableMap.<String, WorkStep>builder()
 			.putAll(workStepMap);
 
@@ -34,6 +74,17 @@ public class PluginService {
 		}
 
 		workStepMap = builder.build();
+	}
+
+	public void registerWorkStepInterceptorPlugins(ImmutableList<WorkStepInterceptor> workStepInterceptors) {
+		ImmutableMap.Builder<String, WorkStepInterceptor> builder = ImmutableMap.<String, WorkStepInterceptor>builder()
+			.putAll(workStepInterceptorMap);
+
+		for (WorkStepInterceptor workStepInterceptor : workStepInterceptors) {
+			builder.put(workStepInterceptor.getClass().getName(), workStepInterceptor);
+		}
+
+		workStepInterceptorMap = builder.build();
 	}
 
 	public WorkStep getWorkStep(String name) {
@@ -45,6 +96,6 @@ public class PluginService {
 	}
 
 	public Iterable<WorkStepInterceptor> getWorkStepInterceptors() {
-		return ImmutableList.of();
+		return workStepInterceptorMap.values();
 	}
 }
